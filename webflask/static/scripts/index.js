ae7fa617f2f32f273654631daa8f51edc93ff718
pizzosta <pizzostor@gmail.com>
2023-10-11 20:39:36 +0100

    Push

--- webflask/static/scripts/index.js
+++ webflask/static/scripts/index.js
@@ -6,3 +6,22 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+// Set the target date and time (5 minutes from now)
+var targetDate = new Date();
+targetDate.setMinutes(targetDate.getMinutes() + 1);
+
+function updateTimer() {
+  var now = new Date();
+  var timeLeft = targetDate - now;
+
+  if (timeLeft <= 0) {
+    document.getElementById('countdown').innerHTML = "Auction Ended";
+  } else {
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    document.getElementById('countdown').innerHTML = ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+  }
+}
+
+setInterval(updateTimer, 1000);
