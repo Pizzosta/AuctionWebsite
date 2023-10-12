8a4fd15df3f425c02fdc5c71e250378c4dfd9edb
pizzosta <pizzostor@gmail.com>
2023-10-12 01:14:35 +0100

    Front End

--- webflask/auth.py
+++ webflask/auth.py
@@ -69,6 +69,8 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for("views.home"))
+            # return redirect(url_for("views.home"))
+            return redirect(url_for("views"))
+
 
     return render_template("sign_up.html", user=current_user)
