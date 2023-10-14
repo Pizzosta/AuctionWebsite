450fb26b487dbc88a5031ed77e77cc8aa0755b66
pizzosta <pizzostor@gmail.com>
2023-10-14 04:53:02 +0100

    Fixed bid validation

--- webflask/views.py
+++ webflask/views.py
@@ -5,6 +5,7 @@ from flask_uploads import UploadSet, IMAGES
 from werkzeug.utils import secure_filename
 from webflask.models import User, Image, Auction, Bid
 from webflask import db
+from sqlalchemy.orm import validates
 
 views = Blueprint('views', __name__)
 
@@ -57,70 +58,96 @@ def admin_panel():
         print("Images:", image)
 
         if title and description and start_time and end_time and starting_bid and bid_amount:
-            # Handle the Auction form submission
-            # Create the Auction object and add it to the database
-            auction = Auction(title=title, description=description,
-                              start_time=start_time, end_time=end_time,
-                              starting_bid=starting_bid, user_id=current_user.id)
-            db.session.add(auction)
-            db.session.commit()
-            flash('Auction created successfully!', category='success')
+            # Validate the end time and starting bid
+            @validates('end_time')
+            def validate_end_time(end_time, start_time):
+                if end_time <= start_time:
+                    raise ValueError("End time must be after start time.")
+                return end_time
+            
+            @validates('starting_bid')
+            def validate_starting_bid(starting_bid):
+                starting_bid = float(starting_bid)
+                if starting_bid <= 0:
+                    raise ValueError("Starting bid must be a positive value.")
+                return starting_bid
+
+            try:
+                end_time = validate_end_time(end_time, start_time)
+                starting_bid = validate_starting_bid(starting_bid)
+            except ValueError as e:
+                # Create a flash message for validation error
+                flash(str(e), 'danger')
+            else:
+                # Handle the Auction form submission
+                # Create the Auction object and add it to the database
+                auction = Auction(title=title, description=description,
+                                  start_time=start_time, end_time=end_time,
+                                  starting_bid=starting_bid, user_id=current_user.id)
+                db.session.add(auction)
+                db.session.commit()
+                flash('Auction created successfully!', category='success')
+
+                if image:
+                    # Get a list of uploaded files
+                    for uploaded_file in image:
+                        if uploaded_file:
+                            # Sanitize the filename using secure_filename
+                            filename = secure_filename(uploaded_file.filename)
+                            # Save the sanitized filename
+                            filename = uploaded_images.save(
+                                uploaded_file, name=filename)
+
+                            # Create an Image object and associate it with the current user and auction
+                            image = Image(
+                                filename=filename, user_id=current_user.id, auction_id=auction.id)
+                            db.session.add(image)
+                            db.session.commit()
+                            flash('Images submitted successfully!',
+                                  category='success')
+                        else:
+                            flash('No images selected!', category='danger')
 
         if bid_amount:
             # Handle the Bid form submission
             bid_amount = float(bid_amount)
+            # Validate the bid amount
+            if bid_amount >= starting_bid:
+                def get_appropriate_auction():
+                    # Select the last created auction based on the 'created_at' attribute (assuming it's a DateTime field)
+                    auction = Auction.query.join(User).filter(
+                        User.is_admin == True, Auction.deleted == False).order_by(Auction.created_at.desc()).first()
 
-            def get_appropriate_auction():
-                # Select the last created auction based on the 'created_at' attribute (assuming it's a DateTime field)
-                auction = Auction.query.join(User).filter(User.is_admin == True, Auction.deleted == False).order_by(Auction.created_at.desc()).first()
-
-                print("Appropriate Auction:", auction)
-                return auction
-            # Create the Bid object and add it to the database
-            auction = get_appropriate_auction()  # Get the appropriate auction
-            if auction:
-                bid = Bid(amount=bid_amount, user_id=current_user.id,
-                          auction_id=auction.id)
-                db.session.add(bid)
-                db.session.commit()
-                flash('Bid placed successfully!', category='success')
+                    print("Appropriate Auction:", auction)
+                    return auction
+                # Create the Bid object and add it to the database
+                auction = get_appropriate_auction()  # Get the appropriate auction
+                if auction:
+                    bid = Bid(amount=bid_amount, user_id=current_user.id,
+                            auction_id=auction.id)
+                    db.session.add(bid)
+                    db.session.commit()
+                    flash('Bid placed successfully!', category='success')
             else:
                 flash(
-                    'No active auctions available. Create a new auction first.', category='danger')
+                    'Bid amount must be equal to or greater than the starting bid.', category='danger')
 
-        if image:
-            # Get a list of uploaded files
-            for uploaded_file in image:
-                if uploaded_file:
-                    # Sanitize the filename using secure_filename
-                    filename = secure_filename(uploaded_file.filename)
-                    # Save the sanitized filename
-                    filename = uploaded_images.save(
-                        uploaded_file, name=filename)
-
-                    # Create an Image object and associate it with the current user and auction
-                    image = Image(
-                        filename=filename, user_id=current_user.id, auction_id=auction.id)
-                    db.session.add(image)
-                    db.session.commit()
-                    flash('Images submitted successfully!', category='success')
-                else:
-                    flash('No images selected!', category='danger')
 
     return render_template('admin.html', user=current_user, username=current_user.username, uploaded_images=uploaded_images)
+
 
 @views.route('/delete-auction/<int:id>', methods=['POST'])
 @login_required
 def delete_auction(id):
     auction = Auction.query.get(id)
-    #auction = Auction.query.join(User).filter(User.is_admin == True, Auction.deleted == False).order_by(Auction.created_at.desc()).first()
+    # auction = Auction.query.join(User).filter(User.is_admin == True, Auction.deleted == False).order_by(Auction.created_at.desc()).first()
 
     # Check if the auction exists and belongs to the current user
     if auction and auction.user_id == current_user.id:
         db.session.delete(auction)
         db.session.commit()
-        #flash('Auction deleted successfully!', category='success')
+        # flash('Auction deleted successfully!', category='success')
         return '', 204  # Return 'No Content' status for successful deletion
     else:
-        #flash('Unauthorized to delete this auction.', category='danger')
+        # flash('Unauthorized to delete this auction.', category='danger')
         return 'Unauthorized', 401  # Return 'Unauthorized' status
